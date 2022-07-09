dedfb7109a4f1b0a988ca2dccfdf849b269b5f24
Dmitriy Narishkin <97532271+snow625@users.noreply.github.com>
2022-07-09 18:57:40 +0300

    Updates

---
+++ static/js/93.67a4c26f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[93],{3093:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(501),o=t(184),c=function(){return(0,o.jsx)(o.Fragment,{children:(0,o.jsxs)("h2",{children:["404 Page not found ",(0,o.jsx)(r.rU,{to:"/",children:"Go Home?"})]})})}}}]);
+//# sourceMappingURL=93.67a4c26f.chunk.js.map
